00643c09ea481326dda5f6a7fdf683b4cac01104
laurimyllyvirta <laurimyllyvirta@yahoo.com>
2020-11-19 10:33:03 +0200

    improve plot appearance

--- analysis.targets.R
+++ analysis.targets.R
@@ -55,13 +55,16 @@ m.quarterly %<>% filter(source=='hourly') %>%
                         value))
 
 mean4=function(x) ifelse(length(x)==4,mean(x), NA)
+targetmean=function(df) {
+  df %>% group_by(CityEN, Province, keyregion2018) %>% summarise_at('value', mean4) %>%
+    filter(!is.na(value))
+}
+
 bind_rows(m.quarterly %>% filter(source == 'hourly', Q %in% c(2020.1, 2020.2, 2020.3)),
-          m.quarterly %>% filter(source == 'target', Q %in% c(2020.4))) %>%
-  group_by(CityEN, Province, keyregion2018) %>% summarise_at('value', mean4) -> targetmeans.Q4
+          m.quarterly %>% filter(source == 'target', Q %in% c(2020.4))) %>% targetmean -> targetmeans.Q4
 
 bind_rows(m.quarterly %>% filter(source == 'hourly', Q %in% c(2020.2, 2020.3)),
-          m.quarterly %>% filter(source == 'target', Q %in% c(2020.4, 2021.1))) %>%
-  group_by(CityEN, Province, keyregion2018) %>% summarise_at('value', mean4) -> targetmeans.Q1
+          m.quarterly %>% filter(source == 'target', Q %in% c(2020.4, 2021.1))) %>% targetmean -> targetmeans.Q1
 
 
 #One year running mean plots with the target for end of Q4 and Q1 marked as points, and linear path from latest value to targets
